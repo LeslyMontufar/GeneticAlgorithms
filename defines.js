7bea5eb4dc8fca6d609f35903e05b8f0bca18718
Lesly <leslymontufarj@gmail.com>
2025-04-15 23:07:03 -0300

    crossover sem problemas

--- defines.js
+++ defines.js
@@ -1,68 +1,68 @@
 let pontosPisces = false;
 
 function adicionarConstelacao() {
-    pontos = [
-        { id: "1", x: 1050, y: 400 },  
-        { id: "2", x: 1170, y: 420 },
-        { id: "3", x: 1250, y: 460 },
-        { id: "4", x: 1330, y: 480 },
-        { id: "5", x: 1400, y: 440 },
-        { id: "6", x: 1470, y: 470 },
-        { id: "7", x: 1550, y: 500 },
-        { id: "8", x: 1170, y: 360 },
-        { id: "9", x: 1250, y: 300 },
-        { id: "10", x: 1330, y: 260 },
-        { id: "11", x: 1400, y: 230 },
-        { id: "12", x: 1470, y: 200 },
-        { id: "13", x: 970, y: 380 },
-        { id: "14", x: 890, y: 360 },
-        { id: "15", x: 810, y: 340 },
-        { id: "16", x: 750, y: 300 },
-        { id: "17", x: 690, y: 250 },
-        { id: "18", x: 630, y: 220 },
-        { id: "19", x: 590, y: 200 },
-        { id: "20", x: 550, y: 180 },
-        { id: "21", x: 530, y: 160 }
-      ];
+  pontos = [
+    { id: "1", x: 1050, y: 400 },
+    { id: "2", x: 1170, y: 420 },
+    { id: "3", x: 1250, y: 460 },
+    { id: "4", x: 1330, y: 480 },
+    { id: "5", x: 1400, y: 440 },
+    { id: "6", x: 1470, y: 470 },
+    { id: "7", x: 1550, y: 500 },
+    { id: "8", x: 1170, y: 360 },
+    { id: "9", x: 1250, y: 300 },
+    { id: "10", x: 1330, y: 260 },
+    { id: "11", x: 1400, y: 230 },
+    { id: "12", x: 1470, y: 200 },
+    { id: "13", x: 970, y: 380 },
+    { id: "14", x: 890, y: 360 },
+    { id: "15", x: 810, y: 340 },
+    { id: "16", x: 750, y: 300 },
+    { id: "17", x: 690, y: 250 },
+    { id: "18", x: 630, y: 220 },
+    { id: "19", x: 590, y: 200 },
+    { id: "20", x: 550, y: 180 },
+    { id: "21", x: 530, y: 160 }
+  ];
 
-    desenharTudo(); // Só desenha fundo + estrelas
-    pontosPisces = true;
+  desenharTudo(); // Só desenha fundo + estrelas
+  pontosPisces = true;
 }
 
 function desenharConstelacao(ordem) {
-    ctx.strokeStyle = "#00bfff";  // Cor das linhas
-    ctx.lineWidth = 2;
-    ctx.shadowColor = "#00bfff";
-    ctx.shadowBlur = 10;
-    
-    ctx.beginPath();
-    for (let i = 0; i < ordem.length; i++) {
-      const ponto = pontos[ordem[i]-1];
-      console.log(ponto)
-      if (i === 0) {
-        ctx.moveTo(ponto.x, ponto.y);
-      } else {
-        ctx.lineTo(ponto.x, ponto.y);
-      }
+  ctx.strokeStyle = "#00bfff";  // Cor das linhas
+  ctx.lineWidth = 2;
+  ctx.shadowColor = "#00bfff";
+  ctx.shadowBlur = 10;
+
+  ctx.beginPath();
+  for (let i = 0; i < ordem.length; i++) {
+    const ponto = pontos[ordem[i] - 1];
+    if (i === 0) {
+      ctx.moveTo(ponto.x, ponto.y);
+    } else {
+      ctx.lineTo(ponto.x, ponto.y);
     }
-  
-    // Fecha o ciclo (último ponto → primeiro)
-    const primeiro = pontos[ordem[0]-1];
-    ctx.lineTo(primeiro.x, primeiro.y);
-    ctx.stroke();
-  
-    ctx.shadowBlur = 0;  // Limpa a sombra após desenhar
+  }
+
+  // Fecha o ciclo (último ponto → primeiro)
+  const primeiro = pontos[ordem[0] - 1];
+  ctx.lineTo(primeiro.x, primeiro.y);
+  ctx.stroke();
+
+  ctx.shadowBlur = 0;  // Limpa a sombra após desenhar
 }
 
-function resolverConstelacao() {
-    // ordem = [
-    //     "21", "20", "19", "18", "17", "16", "15", "14", "13", "1",
-    //     "2", "3", "4", "5", "6", "7",
-    //     "8", "9", "10", "11", "12"
-    // ];
-
-    const iterations = document.querySelector('input[name="epochs"]').value;
-    const populationSize = document.querySelector('input[name="population"]').value;
-    ordem = geneticAlgorithm(iterations, populationSize, pontos);
+async function resolverConstelacao() {
+  // ordem = [
+  //     "21", "20", "19", "18", "17", "16", "15", "14", "13", "1",
+  //     "2", "3", "4", "5", "6", "7",
+  //     "8", "9", "10", "11", "12"
+  // ];
+  
+  const iterations = document.querySelector('input[name="epochs"]').value;
+  const populationSize = document.querySelector('input[name="population"]').value;
+  const pc = document.querySelector('input[name="pc"]').value;
+  ordem = geneticAlgorithm(iterations, populationSize, pc);
 }
 
--- ga_caixeiro_viajante.js
+++ ga_caixeiro_viajante.js
@@ -1,18 +1,17 @@
-function fitness(x){
+function fitness(x) {
     let dist = 0, dx, dy;
-    for(let i=0; i<pontos.length-1; i+=1){ // x[motor-1] = posicaoparada
-        dx = (pontos[x[i]-1].x - pontos[x[i+1]-1].x);
-        dy = (pontos[x[i]-1].y - pontos[x[i+1]-1].y);
+    for (let i = 0; i < pontos.length - 1; i += 1) { // x[motor-1] = posicaoparada
+        dx = (pontos[x[i] - 1].x - pontos[x[i + 1] - 1].x);
+        dy = (pontos[x[i] - 1].y - pontos[x[i + 1] - 1].y);
         dist += Math.sqrt(dx * dx + dy * dy);
     }
-    dx = (pontos[x[pontos.length-1]-1].x - pontos[x[0]-1].x);
-    dy = (pontos[x[pontos.length-1]-1].y - pontos[x[0]-1].y);
+    dx = (pontos[x[pontos.length - 1] - 1].x - pontos[x[0] - 1].x);
+    dy = (pontos[x[pontos.length - 1] - 1].y - pontos[x[0] - 1].y);
     dist += Math.sqrt(dx * dx + dy * dy);
     return dist;
 }
-
 function randomChromosome(numCities) {
-    const cities = Array.from({ length: numCities }, (_, i) => i+1);
+    const cities = Array.from({ length: numCities }, (_, i) => i + 1);
     for (let i = cities.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [cities[i], cities[j]] = [cities[j], cities[i]];
@@ -20,65 +19,72 @@ function randomChromosome(numCities) {
     return cities;
 }
 
-function fillIndividual(x){
+function fillIndividual(x) {
     let fit = fitness(x);
-    return {x, fit};
+    return { x, fit };
 }
 
-function randomIndividual(ids=10) {
+function randomIndividual(ids = 10) {
     let x = randomChromosome(ids);
-    return fillIndividual(x); 
+    return fillIndividual(x);
 }
 
-function cycleCrossover(parent1, parent2, ids=21) {
+function cycleCrossover(parent1, parent2, ids = 21) {
     let start = parent1.x[0], last = parent2.x[0];
     let index = 0;
     let visited = Array.from({ length: ids }, () => false);
-    console.log("inicio")
+    visited[0] = true;
 
     let count = 0;
-
-    do{
+    
+    do {
         index = parent1.x.indexOf(last);
         last = parent2.x[index];
-        visited[index]=true;
-        console.log(last);
-        count+=1;
-        if(count>30)
+        visited[index] = true;
+        count += 1;
+        if (count > 30) {
             console.log("erro")
-            return 0;
-    }while(last!=start);
-    
+            return;
+        }
+    } while (last != start);
+
     let x1 = [...parent1.x];
     let x2 = [...parent2.x];
-    for(let i=0; i<ids; i++){
-        if(!visited[i]){
-            [x1[i], x2[i]] = [x2[i], x1[i]];
+    for (let i = 0; i < ids; i++) {
+        if (!visited[i]) {
+            x1[i] = parent2.x[i];
+            x2[i] = parent1.x[i];
         }
     }
-    console.log("fim")
+    console.log(`Pais:
+${JSON.stringify(parent1.x)}
+${JSON.stringify(parent2.x)}
+Filhos:
+${JSON.stringify(x1)}
+${JSON.stringify(x2)}
+        `)
 
     return [fillIndividual(x1), fillIndividual(x2)];
 }
 
-function sumFitness(individuos){
+function sumFitness(individuos) {
     return individuos.reduce((acumulador, individuo) => acumulador + individuo.fit, 0);
 }
 
-function tournamentSelection(population, tournamentSize=3) {
+function tournamentSelection(population, tournamentSize = 3) {
     let bestIndex = null;
-    let bestFitness = -Infinity;
+    let bestFitness = +Infinity;
 
     for (let i = 0; i < tournamentSize; i++) {
-        let randomIndex = Math.floor(Math.random() * (population.length-1) + 1); // evita pegar o primeiro individuo, pois ele foi o melhor da geracao passada
+        let randomIndex = Math.floor(Math.random() * (population.length - 6) + 5); // evita pegar o primeiro individuo, pois ele foi o melhor da geracao passada
 
-        if (population[randomIndex].fit > bestFitness) {
+        if (population[randomIndex].fit < bestFitness) {
             bestFitness = population[randomIndex].fit;
             bestIndex = randomIndex;
         }
     }
 
-    return {individuo: population[bestIndex], posicao: bestIndex};
+    return { individuo: population[bestIndex], posicao: bestIndex };
 }
 
 function generatePopulation(popSize, numCities) {
@@ -88,7 +94,6 @@ function generatePopulation(popSize, numCities) {
     while (population.length < popSize) {
         const chromo = randomChromosome(numCities);
         const key = chromo.join(',');
-
         if (!uniqueSet.has(key)) {
             uniqueSet.add(key);
             population.push(fillIndividual(chromo));
@@ -98,19 +103,19 @@ function generatePopulation(popSize, numCities) {
     return population;
 }
 
-async function geneticAlgorithm(iterations = 100, populationSize = 50) { // pontos é var global
+async function geneticAlgorithm(iterations = 100, populationSize = 50, pc = 0.1) { // pontos é var global
     let population = generatePopulation(populationSize, pontos.length);
-    // console.log(population);
-    let pc = 0.1;
+    console.log(population)
     let bestIndividual = null;
     let meanGenerationFit = 0;
 
     let bestIndividualFitArr = [];
-    let meanPopFitArr = [];  
+    let meanPopFitArr = [];
 
-    for (let i = 0; i < iterations; i++) {        
-        for(let j=0; j<Math.floor(populationSize/2); j++){
-            if(Math.random()<pc){
+    for (let i = 0; i < iterations; i++) {
+        console.log(JSON.stringify(population));
+        for (let j = 0; j < Math.floor(populationSize / 2); j++) {
+            if (Math.random() < pc) {
                 let { individuo: parent1, posicao: ip1 } = tournamentSelection(population);
                 let { individuo: parent2, posicao: ip2 } = tournamentSelection(population);
                 let childs = cycleCrossover(parent1, parent2, pontos.length);
@@ -120,14 +125,16 @@ async function geneticAlgorithm(iterations = 100, populationSize = 50) { // pont
         }
 
         bestIndividual = population.sort((a, b) => a.fit - b.fit)[0]; // ordena do menor para o maior
-        // meanGenerationFit = population.reduce((acumulador, individuo) => acumulador + individuo.fit, 0)/populationSize;
+        meanGenerationFit = population.reduce((acumulador, individuo) => acumulador + individuo.fit, 0) / populationSize;
 
-        // bestIndividualFitArr.push(bestIndividual.fit);
-        // meanPopFitArr.push(meanGenerationFit);
+        bestIndividualFitArr.push(bestIndividual.fit);
+        meanPopFitArr.push(meanGenerationFit);
 
         ordem = bestIndividual.x;
         desenharTudo(i+1);
-        await new Promise(resolve => setTimeout(resolve, 50)); // pequena pausa pra animar
+
+        await new Promise(resolve => setTimeout(resolve, 10)); // pequena pausa pra animar
+
     }
     return bestIndividual.x;
 }
