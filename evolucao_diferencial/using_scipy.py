957ed897b73faecc7d84b833272133af86ea9cd2
Lesly <leslymontufarj@gmail.com>
2025-07-17 12:27:30 -0300

    :fire: chore: Remove unused differential evolution implementations and related scripts

--- evolucao_diferencial/using_scipy.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from scipy.optimize import rosen, differential_evolution
-bounds = [(-1,2), (-1, 2)]
-result = differential_evolution(rosen, bounds)
-print(result.x, result.fun)
-# (array([1., 1., 1., 1., 1.]), 1.9216496320061384e-1
