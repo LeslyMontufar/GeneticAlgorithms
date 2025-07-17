8ef264f979d202f6065a6ad6ae92b2594b306f05
Lesly <leslymontufarj@gmail.com>
2025-07-17 07:54:49 -0300

    :sparkles: feat: Update fitness functions and integrate Rosenbrock optimization; enhance differential evolution parameters and add new scripts for testing

--- evolucao_diferencial/fitness_function.py
+++ evolucao_diferencial/fitness_function.py
@@ -8,13 +8,14 @@ def fitness_function(x):
     Returns:
         float: The fitness score of the individual.
     """
-    return Rastrigin(x,-5.12,5.12, 2)  # Assuming genes are integers or floats
+    return Rastrigin(x,-5.12,5.12) 
+    # return Rosenbrock(x, -1, 2) 
 
 from math import cos, pi
 
-def Rastrigin(x, x_linf, x_lsup, n):
+def Rosenbrock(x, x_linf=-1, x_lsup=2):
     """
-    Rastrigin function for optimization.
+    Rosenbrock function for optimization.
     
     Args:
         x_lsup (float): Upper limit for the variables.
@@ -22,9 +23,24 @@ def Rastrigin(x, x_linf, x_lsup, n):
         n (int): Number of variables.
     
     Returns:
+        float: The value of the Rosenbrock function.
+    """
+    n = len(x)  # Ensure n is the length of x
+    x = [xi if x_linf <= xi <= x_lsup else (x_linf if xi < x_linf else x_lsup) for xi in x]  # Clamping values to limits
+    return sum([100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2 for i in range(n-1)])  # Assuming xi are the variables
+
+def Rastrigin(x, x_linf=-5.12, x_lsup=5.12):
+    """
+    Rastrigin function for optimization.
+    
+    Args:
+        x_lsup (float): Upper limit for the variables.
+        x_linf (float): Lower limit for the variables.
+    
+    Returns:
         float: The value of the Rastrigin function.
     """
-    
+    n = len(x) 
     x = [xi if x_linf <= xi <= x_lsup else (x_linf if xi < x_linf else x_lsup) for xi in x]  # Clamping values to limits
     A = 10
     return A * n + sum([(xi**2 - A * cos(2 * pi * xi)) for xi in x])  # Assuming xi are the variables
--- evolucao_diferencial/main.py
+++ evolucao_diferencial/main.py
@@ -1,9 +1,8 @@
 import random
 import numpy as np
-from fitness_function import *
+from fitness_function import Rosenbrock as fitness_function
 
-MAXIMIZAR = False
-LINF, LSUP = -5.12, 5.12
+LINF, LSUP =  -1, 2
 
 class Individual:
     def __init__(self, genes):
@@ -18,9 +17,8 @@ def diferential_evolution(generations, population_size, element_count, crossover
     population = [Individual(np.random.uniform(LINF, LSUP, size=element_count)) for _ in range(population_size)]
     next_population = [] 
     best_individual = None
-    elitism_num = 5
+    elitism_num = 10
     min_mutation_factor = 0.1
-    aexp = (min_mutation_factor/(np.exp(-generations)))
 
     for generation in range(generations):
         next_population = []
@@ -29,9 +27,12 @@ def diferential_evolution(generations, population_size, element_count, crossover
                 [idx for idx in range(population_size) if idx != i], 3)]
 
             # Mutação
-            mutation_factor = aexp* np.exp(-generation) + mutation_factor
-            mutant = np.clip(a + mutation_factor * (b - c), LINF, LSUP)
-            print("\t\ta:", a, "b:", b, "c:", c, "mutant:", mutant)
+            #current_mutation_factor = -(mutation_factor - min_mutation_factor)* np.exp(generation-generations) + mutation_factor
+            # current_mutation_factor = mutation_factor - (mutation_factor - min_mutation_factor) * (generation / generations)
+            current_mutation_factor = mutation_factor
+
+            mutant = np.clip(a + current_mutation_factor * (b - c), LINF, LSUP)
+            # print("\t\ta:", a, "b:", b, "c:", c, "mutant:", mutant)
 
             # Crossover
             cross_points = np.random.rand(element_count) < crossover_rate
@@ -40,25 +41,30 @@ def diferential_evolution(generations, population_size, element_count, crossover
             trial = Individual(np.where(cross_points, mutant, population[i].genes))            
 
             # Seleção 
-            if (trial.fitness > population[i].fitness) ^ MAXIMIZAR: # XOR
+            if trial.fitness < population[i].fitness: # XOR
                 next_population.append(trial)
                 if i<elitism_num:
                     next_population.append(population[i])  # Elitism: keep the best individuals
             else:
                 next_population.append(population[i])
-                 
+        
+        next_population.sort(key=lambda ind: ind.fitness, reverse=False)  # Sort by fitness      
         population = next_population[:population_size]  # Ensure population size remains constant
-        population.sort(key=lambda ind: ind.fitness, reverse=MAXIMIZAR)  # Sort population by fitness
+        population.sort(key=lambda ind: ind.fitness, reverse=False)  # Sort population by fitness
         best_individual = population[0]  # Best individual is the first one after sorting
-        print(f"Generation {generation+1}: Best individual: {best_individual.genes}  Best fitness = {best_individual.fitness} Worst fitness = {population[-1].fitness}")      
-    
+        print(f"Generation {generation+1}: Best individual: {best_individual.genes}  Best fitness = {best_individual.fitness} \tmutation_factor = {current_mutation_factor} Worst fitness = {population[-1].fitness}")      
+
+        if best_individual.fitness < 1e-6:
+            print("Early stopping: Fitness threshold reached.")
+            break
+        
     return best_individual
 
 if __name__ == "__main__":
     mutation_factor = 0.5
-    crossover_rate = 0.7
+    crossover_rate = 0.9
     generations = 100
-    population_size = 10
+    population_size = 100
     element_count = 2 # x y of the Rastrigin function
     best_individual = diferential_evolution(generations, population_size, element_count, crossover_rate, mutation_factor)
     print("Best individual:", best_individual.genes, "Fitness:", best_individual.fitness)
